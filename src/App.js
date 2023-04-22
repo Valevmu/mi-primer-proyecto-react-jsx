f34965e35585b4ce3d671ec0f0ff44227dda969c
Valevmu <madridvalentina00@gmail.com>
2023-04-22 18:48:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Saludo from "./componentes/Saludo.jsx";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Saludo />
+     
+    </div>
+  );
+}
+
+export default App;
